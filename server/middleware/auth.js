87133ef79c19ccc09cc2c5d9c239e34f3271ee6b
Hack Reactor Student <student@hackreactor.com>
2017-11-07 20:02:23 -0800

    Pseudocoded more createSession logic

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,31 +1,60 @@
 const models = require('../models');
 const Promise = require('bluebird');
 const parseCookies = require('./cookieParser');
+//GOAL: SET A NEW COOKIE ON THE RESPONSE WHEN A SESSION IS INITIALIZED
+//TODO
+  //1 Identify when a session is initialized
+  //... check if the cookie matches a pre-existing user
+  //2 Generate cookies re-using our pre-existing hashing function
+    //3 For the new user
+
 
 module.exports.createSession = (req, res, next) => {
-  console.log('createSession invoked by test!: here\'s the req: ', req);
-  var hashed;
   req.session = {};
-  var storage = [];
-  if (Object.keys(req.session).length === 0) {
-    models.Sessions.create()
+  if (req.cookies) {
+    //get req's session id
+    models.Sessions.get({id: result.insertId})
+      //if req's session id exists
       .then(result => {
-        return models.Sessions.get({id: result.insertId});
-      })
-      .then(result => {
-        req.session = {id: result.id, userId: result.userId, hash: result.hash};
-        console.log('Do I exist, really?', req.session);
-        next();
-      })
-      .catch(error => {
-        console.error(error);
+        if (req.session.userId) {
+          if (req.session.userId !== result.insertId) {
+            next();
+          }
+        //query db for session id
+          //store query session id
+          //compare stored id against req's session id
+            //if they are equal
+              //set the req's session to the session
+              //call next
+            //if they aren't equal
+              //USE THE BELOW CODE
+              //set the req's session to the newly generated session
+              //call next
+        }
       });
+      //if req's session id does not exist
+        //USE THE BELOW CODE
+          //set the reqs session to the newly generated session
+          //call next
+      //USE THE BELOW CODE
   }
-  console.log('am I filled?', storage);
-
-  //Call session's get
-    //Pass in options TODO: grab userId prop from req
-    
+  // if (Object.keys(req.session).length === 0) {
+  //   models.Sessions.create()
+  //     .then(result => {
+  //       return models.Sessions.get({id: result.insertId});
+  //     })
+  //     .then(result => {
+  //       req.session = {id: result.id, userId: result.userId, hash: result.hash};
+  //       session.cookies['shortlyid'] = result.hash;
+  //       next();
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // } else {
+  //   req.session = session
+  //   next();
+  // }
 };
 
 /************************************************************/
